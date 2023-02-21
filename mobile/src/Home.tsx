b4b99f0e993c8c4304cd842859eea571cfe30981
herbert de sousa <sousaherbert138@gmail.com>
2023-02-21 09:50:53 -0300

    setup

---
+++ mobile/src/Home.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export function Home() {
+  return (
+    <View>
+      <Text>
+        asdadassadasdad asd ad asd a asd ad asd asdasd asd as dasd asd asdad
+      </Text>
+    </View>
+  );
+}
